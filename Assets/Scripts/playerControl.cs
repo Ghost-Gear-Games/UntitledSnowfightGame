721bc35d9d9aad6bc23cda496b510fbb5d8715c2
GhostGearGames <firecardmaster@gmail.com>
2022-12-30 01:17:07 -0500

    worked on attack action
    *renamed movement script to player control script
    +switch statement inside player controller for attacks
    
    still getting basic attack to work

--- Assets/Scripts/playerControl.cs
+++ Assets/Scripts/playerControl.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TopDown2DMovement : MonoBehaviour
+public class playerControl : MonoBehaviour
 {
-
-    public Vector2 movementInput;
     public float speed = 5;
-    public Rigidbody2D rb2D;
     public bool attacking;
+    public Vector2 movementInput;
+    public Vector3 snowballDistance;
     public Animator animator;
+    public Rigidbody2D rb2D;
+    public GameObject snowball;
+    private void Start()
+    {
 
+    }
     void Update()
     {
         //Input is collected and set to our variable every frame Input in this case is also a class just like this scripts 'TopDown2DMovement' script
@@ -32,17 +36,50 @@ public class TopDown2DMovement : MonoBehaviour
         true if the button is currently bieng held down on this frame
         false if it isnt
         */
-        attacking = Input.GetButtonDown("Fire1");
+        attacking = Input.GetButtonDown("Fire3");
 
         animator.SetFloat("XInput", movementInput.x);
         animator.SetFloat("YInput", movementInput.y);
         animator.SetFloat("speed", movementInput.sqrMagnitude);
         animator.SetBool("attacking", attacking);
+        if (attacking)
+        {
+            startAttack("base");
+        }
+        snowballDistance = new Vector3(movementInput.x, movementInput.y, 0);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         rb2D.MovePosition(rb2D.position + movementInput * speed * Time.fixedDeltaTime);
+    }
+    void startAttack(string attack)
+    {
+
+        switch (attack)
+        {
+            case "base":
+                //BASE ATTACK TODO
+                //start transition animation
+                var baseSnowball = Instantiate(snowball, this.transform);
+                //grow 'bullet'
+                baseSnowball.transform.position = this.transform.position + snowballDistance;
+                baseSnowball.transform.localScale += new Vector3 (Time.deltaTime, Time.deltaTime, 0);
+                //stop 'bullet' growth on player input or at certain size
+                //start new attack holding animation
+                //on player input release 'bullet' in direction player is moving
+                //delete 'bullet' after a set time or/and on collision
+                break;
+            case "attack2":
+                break;
+            case "attack3":
+                break;
+            case "attack4":
+                break;
+            case "attack5":
+                break;
+        }
+
     }
 }
