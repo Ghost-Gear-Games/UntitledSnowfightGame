ce389d3b618e5d974a692d15a687de0bc73896af
abundant920cloud <bajacorl1225@gmail.com>
2023-01-19 15:31:34 -0700

    start of wave spawn system

---
+++ Assets/Scripts/waves.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waves : MonoBehaviour
+{
+    public int enemyCount;
+    public int enemyLimit;
+    public float spawnInterval;
+    public GameObject enemy;
+}
