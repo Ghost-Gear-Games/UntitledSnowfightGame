97349a1113c7460abf0f13470f36c4a9f8af63a2
GhostGearGames <firecardmaster@gmail.com>
2023-01-22 08:49:37 -0700

    fixed up waveControl
    *got rid of any errors in waveControl.cs to prevent safe mode requirement

--- Assets/Scripts/waveControl.cs
+++ Assets/Scripts/waveControl.cs
@@ -6,24 +6,26 @@ public class waveControl : MonoBehaviour
 {
     public enum waveState { waiting, spawning }
     public waveState currentWaveState = waveState.waiting;
+    public int currentWave;
     public waves[] waves_;
-    IEnumerator waveSpawner() {
+    IEnumerator waveSpawner()
+    {
         if (currentWaveState == waveState.waiting)
         {
             Debug.Log("Wait for 10 seconds");
-            yield return new WaitForSeconds(waves_.waitTime);
+            yield return new WaitForSeconds(waves_[currentWave].waitTime);
             currentWaveState = waveState.spawning;
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
