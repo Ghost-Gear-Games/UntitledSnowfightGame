9e64a55fe0abaf651ebc1bb3b7b3221c3f7aa84e
abundant920cloud <bajacorl1225@gmail.com>
2023-01-26 15:51:41 -0700

    worked on wave control again
    made wave array

--- Assets/Scripts/waveControl.cs
+++ Assets/Scripts/waveControl.cs
@@ -6,8 +6,18 @@ public class waveControl : MonoBehaviour
 {
     public enum waveState { waiting, spawning }
     public waveState currentWaveState = waveState.waiting;
-    public int currentWave;
-    public waves[] waves_;
+    public int currentWave = 0;
+    public waveList[] waves_;
+    public GameObject enemy1;
+    [System.Serializable]
+    public class waveList
+    {
+        public int enemyCount;
+        public int enemyLimit;
+        public float spawnInterval;
+        public GameObject enemy;
+        public float waitTime;
+    }
     IEnumerator waveSpawner()
     {
         if (currentWaveState == waveState.waiting)
@@ -15,6 +25,16 @@ public class waveControl : MonoBehaviour
             Debug.Log("Wait for 10 seconds");
             yield return new WaitForSeconds(waves_[currentWave].waitTime);
             currentWaveState = waveState.spawning;
+        }
+        if (currentWaveState == waveState.spawning)
+        {
+            for (int i = 0; i < waves_[currentWave].enemyLimit; i++)
+            {
+                waves_[currentWave].enemyCount++;
+                Instantiate(enemy1);
+                currentWave += 1;
+            }
+
         }
     }
     // Start is called before the first frame update
